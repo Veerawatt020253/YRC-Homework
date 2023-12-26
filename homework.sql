bbb0c5655d21dc436d424b014992a56867babb7c
Veerawatt020253 <53421@yupparaj.ac.th>
2023-12-26 13:06:32 +0700

    .

--- homework.sql
+++ homework.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 22, 2023 at 04:33 PM
+-- Generation Time: Dec 26, 2023 at 07:03 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -20,27 +20,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `homework`
 --
-
--- --------------------------------------------------------
-
---
--- Table structure for table `admin`
---
-
-CREATE TABLE `admin` (
-  `id` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `surname` varchar(255) NOT NULL,
-  `username` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `admin`
---
-
-INSERT INTO `admin` (`id`, `name`, `surname`, `username`, `password`) VALUES
-(1, 'Veerawatt', 'Kanthiphan', 'admin', 'Nice2250');
 
 -- --------------------------------------------------------
 
@@ -68,15 +47,22 @@ CREATE TABLE `member` (
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `name` varchar(255) NOT NULL,
-  `surname` varchar(255) NOT NULL
+  `surname` varchar(255) NOT NULL,
+  `role` varchar(255) NOT NULL,
+  `r_name` varchar(255) NOT NULL,
+  `r_surname` varchar(255) NOT NULL,
+  `r_nickname` varchar(255) NOT NULL,
+  `r_class` varchar(255) NOT NULL,
+  `r_number` varchar(255) NOT NULL,
+  `r_student_num` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `member`
 --
 
-INSERT INTO `member` (`id`, `username`, `password`, `name`, `surname`) VALUES
-(1, 'Nice2250', 'Nice2250', 'วีราวรรธนุ์', 'กันธิพันธ์');
+INSERT INTO `member` (`id`, `username`, `password`, `name`, `surname`, `role`, `r_name`, `r_surname`, `r_nickname`, `r_class`, `r_number`, `r_student_num`) VALUES
+(5, 'Nice2250', 'Nice2250', 'Veerawattt', 'Kanthiphan', 'admin', '', '', '', '', '', '');
 
 -- --------------------------------------------------------
 
@@ -102,18 +88,45 @@ CREATE TABLE `todo` (
   `id` int(11) NOT NULL,
   `content` varchar(255) NOT NULL,
   `status` varchar(255) NOT NULL,
+  `owner` varchar(255) NOT NULL,
+  `time` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `viewer`
+--
+
+CREATE TABLE `viewer` (
+  `id` int(11) NOT NULL,
+  `time` varchar(255) NOT NULL,
+  `value` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `web`
+--
+
+CREATE TABLE `web` (
+  `id` int(11) NOT NULL,
+  `leader` varchar(255) NOT NULL,
+  `class` varchar(255) NOT NULL,
   `owner` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Indexes for dumped tables
+-- Dumping data for table `web`
 --
 
+INSERT INTO `web` (`id`, `leader`, `class`, `owner`) VALUES
+(1, 'Test Leader', '2/20', 'Veerawatt Kanthiphan');
+
 --
--- Indexes for table `admin`
+-- Indexes for dumped tables
 --
-ALTER TABLE `admin`
-  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `homework`
@@ -140,38 +153,56 @@ ALTER TABLE `todo`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- Indexes for table `viewer`
 --
+ALTER TABLE `viewer`
+  ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT for table `admin`
+-- Indexes for table `web`
 --
-ALTER TABLE `admin`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+ALTER TABLE `web`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
 
 --
 -- AUTO_INCREMENT for table `homework`
 --
 ALTER TABLE `homework`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `member`
 --
 ALTER TABLE `member`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `news`
 --
 ALTER TABLE `news`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `todo`
 --
 ALTER TABLE `todo`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `viewer`
+--
+ALTER TABLE `viewer`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=567;
+
+--
+-- AUTO_INCREMENT for table `web`
+--
+ALTER TABLE `web`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
